73dea17c1616eb7c3d13bcb99a218e3473175db3
Gary James <gjames@edgetechacademy.edu>
2020-02-11 07:44:10 -0600

    added the titanic router to the app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var prodRouter = require('./routes/products');
-
+var titanicRouter = require('./routes/titanic');
 var app = express();
 
 // view engine setup
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', prodRouter);
+app.use('/titanic', titanicRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
